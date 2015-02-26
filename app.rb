29c8c610e37807969e04fefe1146b7f2dab2ddd7
Joshua Walton <joshua.walton@sparcedge.com>
2015-02-26 16:40:43 -0500

    properties work

--- app.rb
+++ app.rb
@@ -2,13 +2,14 @@ require 'aws-sdk'
 require 'net/http'
 require 'net/https'
 require 'uri'
+config_file '/properties.yml'
 
 get '/getEnvironmentBadge' do
 	elasticbeanstalk = Aws::ElasticBeanstalk::Client.new(
-		region: 'us-west-2',
-	  	access_key_id: 'AKIAJU3P2SJCH5UOBDXA',
-  	  	secret_access_key: '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')
-  	ebsResp = elasticbeanstalk.describe_environments(environment_ids: ["e-y2xhmkgxqf"])	
+		region: settings.aws.region,
+	  	access_key_id: settings.aws.access_key_id,
+  	  	secret_access_key: settings.aws.secret_access_key)
+  	ebsResp = elasticbeanstalk.describe_environments(environment_ids: [settings.aws.environment.id])	
   		
 	envName = ebsResp.environments.first.environment_name.gsub! '-', '_'
   	envStatus = ebsResp.environments.first.status.downcase.capitalize 
@@ -21,6 +22,6 @@ get '/getEnvironmentBadge' do
 	imgrequest = Net::HTTP::Get.new(uri.request_uri)
 	imgresponse = http.request(imgrequest)
     content_type 'image/svg+xml'
-	cache_control 'no-cache'
+	cache_control settings.options.cachecontrol
 	imgresponse.body             # => The body (HTML, XML, blob, whatever)  
 end
