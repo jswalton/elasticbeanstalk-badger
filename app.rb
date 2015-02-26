922c746ad5107fa4bb71887f69d79df58b996f80
Joshua Walton <joshua.walton@sparcedge.com>
2015-02-26 17:17:03 -0500

    fix settings

--- app.rb
+++ app.rb
@@ -2,14 +2,16 @@ require 'aws-sdk'
 require 'net/http'
 require 'net/https'
 require 'uri'
-config_file '/properties.yml'
+require 'sinatra/config_file'
+
+config_file 'properties.yml'
 
 get '/getEnvironmentBadge' do
 	elasticbeanstalk = Aws::ElasticBeanstalk::Client.new(
-		region: settings.aws.region,
-	  	access_key_id: settings.aws.access_key_id,
-  	  	secret_access_key: settings.aws.secret_access_key)
-  	ebsResp = elasticbeanstalk.describe_environments(environment_ids: [settings.aws.environment.id])	
+		region: settings.aws['region'],
+	  	access_key_id: settings.aws['access_key_id'],
+  	  	secret_access_key: settings.aws['secret_access_key'])
+  	ebsResp = elasticbeanstalk.describe_environments(environment_ids: [settings.aws['environment']['id']])	
   		
 	envName = ebsResp.environments.first.environment_name.gsub! '-', '_'
   	envStatus = ebsResp.environments.first.status.downcase.capitalize 
@@ -22,6 +24,6 @@ get '/getEnvironmentBadge' do
 	imgrequest = Net::HTTP::Get.new(uri.request_uri)
 	imgresponse = http.request(imgrequest)
     content_type 'image/svg+xml'
-	cache_control settings.options.cachecontrol
+	cache_control settings.options['cachecontrol']
 	imgresponse.body             # => The body (HTML, XML, blob, whatever)  
 end
